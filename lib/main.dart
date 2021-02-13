d2782880bf7136aa979fc901bc75ab59fe44ebc7
cbooncoder123 <chanok_boon@hotmail.com>
2021-02-13 19:38:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:ShoeShop2/pages/home_page.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
